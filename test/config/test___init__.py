c7333781e9fac4b911a28fe3ee5286c4cc780c29
Francisco Bórquez <fborquez@outlook.com>
2022-10-17 23:29:07 -0300

    feature: getconfig test suite

---
+++ test/config/test___init__.py
@@ -0,0 +1,34 @@
+from unittest import mock, TestCase
+
+from config import getconfig
+from helper.fake_file import fake_ini_file
+
+
+class Test(TestCase):
+    @mock.patch('config.reader.configparser.open')
+    def test_empty_getconfig(self, mock_open):
+        """
+        GIVEN an empty ini file
+        WHEN is read
+        THEN it should return an empty dictionary
+        """
+        mock_open.return_value = fake_ini_file()
+
+        config = getconfig()
+
+        self.assertTrue(type(config) is dict)
+
+    @mock.patch('config.reader.configparser.open')
+    def test_getconfig_with_content(self, mock_open):
+        """
+        GIVEN an empty ini file
+        WHEN is read
+        THEN it should return an empty dictionary
+        """
+        key = "test"
+        content = f"{key}=yes"
+        mock_open.return_value = fake_ini_file(content)
+
+        config = getconfig()
+
+        self.assertTrue(key in config)
